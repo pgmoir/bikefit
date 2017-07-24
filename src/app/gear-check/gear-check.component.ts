29a763aa9cc1586e98473c92132747798331eae9
Phil Moir <pgmoir@gmail.com>
2017-07-24 16:26:20 +0100

    Added basic calculator using default restriction and wheel size. Only front chain ring value can be changed

--- src/app/gear-check/gear-check.component.ts
+++ src/app/gear-check/gear-check.component.ts
@@ -9,6 +9,9 @@ export class GearCheckComponent implements OnInit {
   dateOfBirth: string;
   wheelSize: string;
   chainRingSize: number;
+  recommendation: string;
+  restriction: number;
+  tyreCircumference: number;
 
   constructor() { }
 
@@ -16,6 +19,31 @@ export class GearCheckComponent implements OnInit {
     this.dateOfBirth = '26/01/2003';
     this.wheelSize = '700x23';
     this.chainRingSize = 34;
+    this.recommendation = '';
+  }
+
+  checkGearing() {
+    this.restriction = 6.45;
+    this.tyreCircumference = 2.099;
+    let foundRecommendation = false;
+
+    for (let rearTeeth = 10; rearTeeth < 40; rearTeeth++) {
+      const rollout = this.getRollout(rearTeeth);
+      if (rollout < this.restriction) {
+        this.recommendation = 'The smallest cog on the rear cassette that can be made available on ' +
+          'this bike should have ' + rearTeeth + ' teeth.';
+        foundRecommendation = true;
+        break;
+      }
+      if (!foundRecommendation) {
+        this.recommendation = 'This bike does not seem to be suitable for this cyclist.';
+      }
+    }
+
+  }
+
+  getRollout(rearTeeth) {
+    return this.tyreCircumference * this.chainRingSize / rearTeeth;
   }
 
 }
